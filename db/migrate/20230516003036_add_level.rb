643b8eb74294d134643cf6a01ea7801763bc9895
KendallC09 <kendallconejo00@gmail.com>
2023-05-15 19:43:21 -0600

    Comedian CRUD done

---
+++ db/migrate/20230516003036_add_level.rb
@@ -0,0 +1,5 @@
+class AddLevel < ActiveRecord::Migration[7.0]
+  def change
+    add_column :comedians, :level, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_190550) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_003036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_190550) do
     t.integer "id_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "level"
   end
 
 end
