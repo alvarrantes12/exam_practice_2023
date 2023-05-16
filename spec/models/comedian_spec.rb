87801e4b940f803b40cb452be144c82fa68c0e6a
FerAlvarado99 <maria.alvarado@ucrso.info>
2023-05-16 17:44:26 -0600

    Comedian CRUD done

--- spec/models/comedian_spec.rb
+++ spec/models/comedian_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Comedian, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+ 
+    subject {
+    described_class.new(first_name: "Springfiel",
+                        last_name: "Wikipediasxesrd",
+                        id_number: 279234523
+                         )
+    }
+  
+    it "it validate isnt empty" do
+      expect(subject).to be_valid
+    end
+  
+    it "it validates it is a name" do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+  
+    it "it validates it is a last_name" do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "it validates it is a id" do
+      subject.id_number = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "it validates it is a name with 10 characters" do
+      subject.first_name = "djeuifhrgj"
+      expect(subject).to be_valid
+    end
+  
+    it "it validates it is a last_name with 15 characters" do
+      subject.last_name = "fmeijfrihgendjufg"
+      expect(subject).to_not be_valid
+    end
+  
+    it "it validates it is a id_name with 8-10 characters" do
+      subject.id_number = 123456789
+      expect(subject).to be_valid
+  end
+  end
+
+
+
