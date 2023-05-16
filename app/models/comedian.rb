e33f82df1353418eb3265a41425c84e28dfc016e
AntonyB2002 <abarrantes211@gmail.com>
2023-05-15 19:44:15 -0600

    Comedian CRUD done

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,2 +1,10 @@
 class Comedian < ApplicationRecord
+
+  validates :first_name, presence: true, length: { minimum: 4, maximum: 10 }
+  validates :last_name, presence: true, length: { minimum: 4, maximum: 15 }
+  validates :id_number, presence: true, uniqueness: true, length: {minimum:5, maximum:8}
+  validates :level, presence: true
+
+  enum level: {'low': 0, 'medium': 1, 'high': 2}
+
 end
