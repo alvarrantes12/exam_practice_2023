75928cecc752b4f0881585c735a53b31ed7780f4
Daniel <daniel.vargas@ucrso.info>
2023-05-15 20:30:58 -0600

    Comedian CRUD Done

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,2 +1,9 @@
 class Comedian < ApplicationRecord
+
+    validates :first_name,presence: true, length: { maximum: 10 }
+    validates :last_name,presence: true,length: { maximum: 15 }
+    validates :id_number, presence: true, uniqueness: true, numericality: { greater_than: 9999999, less_than: 10000000000 }
+    validates :level,presence: true
+
+    enum level: {'high': 0, 'medium': 1, 'low': 2}
 end
