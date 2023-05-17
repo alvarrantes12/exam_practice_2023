207860e5af46dae556590552e878f384072daca5
KevinJAM2314 <kevin.arroyomora@ucr.ac.cr>
2023-05-17 17:31:13 -0600

    KJAM-C10724 Comedian CRUD done

---
+++ app/helpers/comedians_helper.rb
@@ -0,0 +1,8 @@
+module ComediansHelper
+
+    def levels
+        Comedian.levels.keys.map do |level|
+            [t("activerecord.attributes.comedian.levels.#{level}"), level]
+        end
+    end
+end
--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,2 +1,8 @@
 class Comedian < ApplicationRecord
+    validates :first_name, presence: true, length: { maximum: 10 }
+    validates :last_name, presence: true, length: { maximum: 15 }
+    validates :id_number, presence: true, uniqueness: true, numericality: { greater_than: 9999999, less_than: 10000000000}
+    validates :level, presence: true
+
+    enum level: {'low': 0, 'half': 1, 'high': 2}
 end
